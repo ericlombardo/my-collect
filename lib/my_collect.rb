0c3ff4fec5c18061c7e3bfc223c1693fb0268d7f
Eric <eric.m.lombardo@gmail.com>
2020-11-17 07:40:00 -0500

    commit tree b3f255a9b089b82a3671bf44aa331e8ff264cc2c

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,15 @@
 require 'pry'
 
 def my_collect(array)
-  i = 0   # set counter to 0
-  new_collect = []    # create empty array for yield collection
-  while i < array.length
-    new_collect << yield(array[i])
-    i += 1
+  if block_given?
+    i = 0   # set counter to 0
+    new_collect = []    # create empty array for yield collection
+    while i < array.length
+      new_collect << yield(array[i])    # pushed return to new array
+      i += 1
+    end
+  else
+    puts "Sorry, no block given."
   end
-  new_collect
+  new_collect   # return new array
 end
