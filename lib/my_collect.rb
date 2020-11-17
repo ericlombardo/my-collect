d4c64beb6d065ab099a89802a5b04c8172cc1151
Eric <eric.m.lombardo@gmail.com>
2020-11-17 07:36:17 -0500

    made #my_collect(array) to yield array to block of code

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+require 'pry'
 
-
+def my_collect(array)
+  i = 0   # set counter to 0
+  new_collect = []    # create empty array for yield collection
+  while i < array.length
+    new_collect << yield(array[i])
+    i += 1
+  end
+  new_collect
+end
